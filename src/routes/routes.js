8f5ea5b81c055ff886a27c9e85d62e587cf5960e
Nicolas Aguilera García <niagar25@gmail.com>
2021-04-15 21:22:55 -0500

    Render the items added

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,11 +3,21 @@ const router = Router();
 
 // const dataApi = require('../public/scripts/API/dataAPI');
 const importCountry = require('../public/scripts/importCountry');
+const { getConection } = require('../../database/database');
 
 router.get('/', (req, res) => {
+  const db = getConection();
+
   importCountry()
     .then((data) => {
-      res.render('pages/index', data);
+      let databaseData = db.get('list').value();
+
+      const infoToSent = {
+        values: data.values,
+        list: databaseData,
+      };
+
+      res.render('pages/index', infoToSent);
     })
     .catch((err) => {
       res.render('pages/index', err);
@@ -15,6 +25,10 @@ router.get('/', (req, res) => {
 });
 
 router.post('/add', (req, res) => {
+  const db = getConection();
+
+  db.get('list').push(req.body).write();
+
   res.status(200).redirect('/');
 });
 
